577d4990933594e19ccc037807ffdc83f28bfa0c
Otavio Senger <senger.otavio@gmail.com>
2025-03-21 17:42:12 -0300

    reponsividade-mobile

--- la-pizza/src/App.jsx
+++ la-pizza/src/App.jsx
@@ -1,6 +1,7 @@
 import logoImg from "./assets/la-pizza/logo.svg";
 import { Card } from "./components/card";
 import LaCumaIMG from "./assets/la-pizza/pizza-1.png";
+import localIMG from "./assets/la-pizza/image-2.png";
 
 export function App() {
   return (
@@ -18,6 +19,46 @@ export function App() {
             description="Lorem ipsum dolor sit, amet consectetur adipisicing elit."
             price="29,90"
           />
+          <Card
+            imgURL={LaCumaIMG}
+            name="La Cuma"
+            description="Lorem ipsum dolor sit, amet consectetur adipisicing elit."
+            price="29,90"
+          />
+          <Card
+            imgURL={LaCumaIMG}
+            name="La Cuma"
+            description="Lorem ipsum dolor sit, amet consectetur adipisicing elit."
+            price="29,90"
+          />
+          <Card
+            imgURL={LaCumaIMG}
+            name="La Cuma"
+            description="Lorem ipsum dolor sit, amet consectetur adipisicing elit."
+            price="29,90"
+          />
+        </div>
+      </div>
+
+      <div className="about">
+        <img src={localIMG} alt="" />
+
+        <div className="description">
+          <p className="d-title">
+            Sobre a La P<span>i</span>
+            <span>z</span>
+            <span>z</span>a
+          </p>
+
+          <p className="history">
+            Bem-vindo ao "La Pizza", o seu refúgio acolhedor para uma
+            experiência de pizzaria autêntica. Em nosso pequeno oásis italiano,
+            cada pedaço de pizza é uma obra-prima artesanal. Sinta-se em casa
+            enquanto o aroma sedutor de massa fresca assa no forno, criando as
+            pizzas mais deliciosas da cidade.
+          </p>
+
+          <button type="button">ler mais sobre</button>
         </div>
       </div>
     </>
--- la-pizza/src/components/card/index.jsx
+++ la-pizza/src/components/card/index.jsx
@@ -1,9 +1,14 @@
 import "./styles.css";
+import {Heart} from "lucide-react"
 
 export function Card({ imgURL, name, description, price }) {
   return (
     <div className="card">
       <img src={imgURL} alt={name} className="product-img" />
+
+    <div className="fav-btn">
+    <Heart size={18} color="#ff3536" fill="#ff3536" className="fav-icon"/>
+    </div>      
       
       <div className="product-details">
       
@@ -11,7 +16,7 @@ export function Card({ imgURL, name, description, price }) {
       <p>{description}</p>
       <strong>R$ {price}</strong>
       </div>
-      <button type="button">Comprar agora</button>
+      <button type="button" className="checkout-btn">Comprar agora</button>
     </div>
   );
 }
