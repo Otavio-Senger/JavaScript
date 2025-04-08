fca8316ab9c1e9f57a6c4c048f7b61a4b89d6f8b
Aluno São Lucas <aluno.saolucas@ensino.ulbra.net>
2025-04-08 17:33:41 -0300

    adicionar

--- shopping-list/src/App.jsx
+++ shopping-list/src/App.jsx
@@ -2,19 +2,30 @@ import { Hearder } from "./components/hearder";
 import { Form } from "./components/form";
 import { Product } from "./components/product";
 import shopppingList from "../data.json";
-
-
-
-
+import { useState } from "react";
 export function App() {
+  const [productList, setProductList] = useState(
+    shopppingList.lista_de_compras
+  );
+
+  function handleAddProduct(newProduct) {
+    setProductList((prevList) => [...prevList, newProduct]);
+  }
+
+  /*   const [serach, setSearch] = useState("");
+  const filteredList = shopppingList.lista_de_compras.filter(({ nome }) =>
+    nome.toLowerCase().includes(serach.toLocaleLowerCase())
+  );
+ */
+
   return (
     <>
       <Hearder />
       <div className="container">
-        <Form />
+        <Form /* onSearch={setSearch} */ onAddProduct={handleAddProduct} />
         <div className="product-list">
-          {shopppingList.lista_de_compras.map(
-            /* desestruturação */
+          {productList.map(
+            // desestruturação
             ({ nome, quantidade, tipo }, index) => (
               <Product
                 key={index}
@@ -24,6 +35,15 @@ export function App() {
               />
             )
           )}
+
+          {/*   {filteredList.map(({ nome, quantidade, tipo }, index) => (
+            <Product
+              key={index}
+              productName={nome}
+              quantity={quantidade}
+              type={tipo}
+            />
+          ))} */}
         </div>
       </div>
     </>
